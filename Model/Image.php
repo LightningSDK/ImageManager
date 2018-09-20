50b4960ba2f27f35cbc609aeeb66ce7d6826ee0e
DAB <dab@dab.la>
2018-09-19 20:51:10 -0500

    added method for building urls

---
+++ Model/Image.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Modules\ImageManager\Model;
+
+class Image {
+    public static function getImage($imageUrl, $size, $format) {
+        // Image manager is enabled, so use the image manager version
+        return '/image?' . http_build_query([
+                'i' => $imageUrl,
+                's' => $size,
+                'f' => $format,
+            ]);
+    }
+}
